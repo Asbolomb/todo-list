22c6d3c4d623eacc752fffabf1c8ffbe701d1b08
Andrew Botham <andrew_botham@hotmail.co.uk>
2022-04-14 17:40:33 +0100

    new list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import {useState} from "react"
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState ([])
+  const [inputText, setInputText] =useState("")
+  const updateInput =(event) =>{
+    setInputText(event.target.value)
+  }
+  const addTask = (event) => {
+    let arr = [...tasks]
+    arr.push(inputText)
+    setTasks(arr)
+    setInputText("")
+  }
+  const removeTask =(index) => {
+    let currentTasks =[...tasks]
+    currentTasks.splice(index, 1)
+    setTasks(currentTasks)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='form'>
+          <h1>MY TO DO LIST</h1>
+           <input onChange={updateInput}/>
+           <button onClick={addTask}>Add Task</button>
+           <h2>{inputText}</h2>
+           {tasks.map((item, index) => {
+              return (
+             <>
+              <h1>{item}</h1>
+             <button onClick={removeTask}>remove</button>
+             </>)
+          })}
     </div>
-  );
+);
+ 
 }
-
 export default App;
